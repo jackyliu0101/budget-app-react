587ece42bf0e1b1f063dfd59dee4ec79a92873a1
Jacky Liu <jackyliu@Jackys-MacBook-Pro-2.local>
2023-04-02 21:33:02 -0700

    experimented with radio buttons and button stylings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useState } from "react"
+import { Button } from './components/Button';
 
 function App() {
+  const [btnType, setBtnType] = useState('default')
+
+  const onRadioBtnChange = (e) => {
+    setBtnType(e.target.value)
+  }
+
   return (
     <div>
-      Hello world
+      <Button type={btnType} title={btnType}/>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="radioBtnTest"
+            value="default"
+            onChange={onRadioBtnChange}
+            defaultChecked />
+            default
+        </label>
+        <label htmlFor="radioBtn2">
+          <input type="radio" name="radioBtnTest" value="primary" onChange={onRadioBtnChange} />
+            primary
+        </label>
+      </div>
     </div>
   )
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Button = ({ type, title }) => {
+  return (
+    <button className={type}>{title}</button>
+  )
+}
+
+Button.defaultProps = {
+  type: 'default'
+}
