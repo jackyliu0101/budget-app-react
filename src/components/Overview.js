a0eb58c8c9cd52deea4c1f2af179d4242aae85b0
Jacky Liu <jackyliu@Jackys-MacBook-Pro-2.local>
2023-04-09 22:55:37 -0700

    minor changes to css

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,7 +6,7 @@ export const Overview = () => {
     <div className="overview-container">
       <div className="overview-content">
         <h2> Your expenses this month </h2>
-        <h3> $500.00 </h3>
+        <h3 className="overview-balance"> $500.00 / $550.00 </h3>
         <Slider currentValue="90" maxValue="100" isDisabled={true}/>
         <hr />
       </div>
