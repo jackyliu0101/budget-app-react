c8a64190ab827182417e9fdbe23f929200045012
Jacky Liu <jackyliu@Jackys-MacBook-Pro-2.local>
2023-04-23 21:11:58 -0700

    started making sidePanel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Overview } from './components/Overview';
+import { SidePanel } from './components/SidePanel';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <SidePanel />
       <Overview />
     </div>
   )
---
+++ src/components/SidePanel.js
@@ -0,0 +1,5 @@
+export const SidePanel = () => {
+  return (
+    <div className="sidePanel"> asdf </div>
+  )
+}
