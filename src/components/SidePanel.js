ff971efeb81595da721035dbf67dc0eefbaa04be
Jacky Liu <jackyliu@Jackys-MacBook-Pro-2.local>
2023-04-23 22:24:01 -0700

    added more styling to sidePanel

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,5 +1,18 @@
 export const SidePanel = () => {
   return (
-    <div className="sidePanel"> asdf </div>
+    <div className="sidePanel">
+      <a className="site-title">MyExpenses</a>
+      <ul>
+        <li>
+          <a href="/overview">Overview</a>
+        </li>
+        <li>
+          <a href="/budget">Budget</a>
+        </li>
+        <li>
+          <a href="/expenses">Expenses</a>
+        </li>
+      </ul>
+    </div>
   )
 }
