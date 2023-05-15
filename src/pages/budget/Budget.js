6b575231c93b869c9582ef98dc835aca67d1484b
Jacky Liu <jackyliu@Jackys-MacBook-Pro-2.local>
2023-05-14 22:33:42 -0700

    wrote method to add budgetItem

--- src/pages/budget/Budget.js
+++ src/pages/budget/Budget.js
@@ -5,7 +5,7 @@ import { Button } from '../../components/Button';
 import { BudgetModal } from './BudgetModal';
 
 export const Budget = () => {
-  const budgetItems = [
+  const dummyBudgetItems = [
     {
       id: 0,
       name: "Miscellaneous",
@@ -19,9 +19,21 @@ export const Budget = () => {
   ]
 
   const [showModal, setShowModal] = useState(false);
+  const [budgetItems, setBudgetItems] = useState(dummyBudgetItems);
 
   const onShowModal = () => {
     setShowModal(true)
+  }
+
+  const addBudgetItem = (budgetInfo) => {
+    let item = {
+      id: Math.floor(Math.random() * 100000 + 5),
+      name: budgetInfo.name,
+      amount: budgetInfo.amount,
+    }
+
+    const updatedBudgetItems = [item, ...budgetItems]
+    setBudgetItems(updatedBudgetItems);
   }
 
   return (
@@ -35,7 +47,7 @@ export const Budget = () => {
         <Button variant="primary" title="Add" onClick={onShowModal} />
       </div>
       <BudgetCardsList budgets={budgetItems}/>
-      <BudgetModal showState={showModal} handleClose={() => setShowModal(false)} />
+      <BudgetModal showState={showModal} handleClose={() => setShowModal(false)} handleAddBudgetItem={addBudgetItem} />
     </>
   )
 }
--- src/pages/budget/BudgetModal.js
+++ src/pages/budget/BudgetModal.js
@@ -1,8 +1,27 @@
 import { Button } from '../../components/Button';
 
-export const BudgetModal = ({ showState, handleClose }) => {
+export const BudgetModal = ({ showState, handleClose, handleAddBudgetItem }) => {
   if (!showState) {
     return null
+  }
+
+
+  const submit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const formJson = Object.fromEntries(formData.entries())
+    const name = formJson.name.trim()
+    const amount = parseFloat(formJson.amount)
+
+    if (name.length > 0 && !isNaN(amount) && amount > 0) {
+      const budget = {
+        name: formJson.name,
+        amount: parseFloat(formJson.amount)
+      }
+      handleAddBudgetItem(budget)
+    }
+
+    handleClose()
   }
 
   return (
@@ -12,18 +31,18 @@ export const BudgetModal = ({ showState, handleClose }) => {
           <h3>Add Budget</h3>
         </div>
         <div className="budget-modal-content">
-          <form className="budget-modal-form">
+          <form className="budget-modal-form" onSubmit={submit}>
             <div className="budget-modal-form-control">
               <label htmlFor="name">Budget Name</label>
-              <input type="text" />
+              <input id="name" name="name" type="text" />
             </div>
             <div className="budget-modal-form-control">
               <label htmlFor="amount">Amount</label>
-              <input type="number" />
+              <input id="amount" name="amount" type="number" step="0.01"/>
             </div>
             <div className="budget-modal-buttons">
               <Button variant="default" title="Close" onClick={handleClose} />
-              <Button variant="primary" title="Save" onClick={() => console.log("Save clicked")} />
+              <Button type="submit" variant="primary" title="Save" />
             </div>
           </form>
         </div>
