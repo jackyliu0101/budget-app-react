6dd1bea240b118386849ad58536bce264c72d63f
Jacky Liu <jackyliu@Jackys-MacBook-Pro-2.local>
2023-07-02 13:36:12 -0700

    added some functionality to submit button

--- src/pages/expenses/AddExpenseForm.js
+++ src/pages/expenses/AddExpenseForm.js
@@ -1,21 +1,32 @@
 import { Button } from '../../components/Button';
 
 export const AddExpenseForm = () => {
+  const submit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formJson = Object.fromEntries(formData.entries());
+    const expense = formJson.expense.trim();
+    const amount = parseFloat(formJson.amount);
+
+    console.log(expense);
+    console.log(amount);
+  }
+
+
   return (
     <div>
       <h3>Add Expense</h3>
-      <form className="add-form add-expense-form">
+      <form className="add-form add-expense-form" onSubmit={submit}>
         <div className="expense-form-control form-control">
           <label htmlFor="expense">Expense</label>
-          <input type="text" placeholder="Enter expense"/>
+          <input id="expense" name="expense" type="text" placeholder="Enter expense"/>
         </div>
         <div className="expense-form-control form-control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" placeholder="Enter amount" />
+          <input id="amount" name="amount" type="number" placeholder="Enter amount" />
         </div>
         <Button type="submit" variant="primary" title="Save" />
       </form>
     </div>
-
   )
 }
